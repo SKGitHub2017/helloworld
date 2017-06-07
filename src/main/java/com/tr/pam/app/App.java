ae651789e72b1bb1cad7bf75d3a6ba356cc0f67b
Somjade Keswongrot <somjade.keswongrot@thomsonreuters.com>
2017-06-07 13:13:58 +0700

    initial commit

---
+++ src/main/java/com/tr/pam/app/App.java
@@ -0,0 +1,13 @@
+package com.tr.pam.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tr/pam/app/AppTest.java
@@ -0,0 +1,40 @@
+package com.tr.pam.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+//mport static java.lang.Sysytem.out;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        System.out.println("Welcome to Dev + Ops");
+        assertTrue( true );
+    }
+}
